dea45c7cc339b2f7ad3dc432dd27f3de5156d79e
jaehyun2yo <68274491+jaehyun2yo@users.noreply.github.com>
2022-11-21 14:18:20 +0900

    main page update

--- dist/index.js
+++ dist/index.js
@@ -193,7 +193,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59432" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65398" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
