407ac0667f41a23c35d77d1024ba129af79c4baf
arch-spatula <shkim516@naver.com>
2022-11-23 10:54:42 +0900

    Create Firebase #2
    Firebase를 셋업 했습니다.

--- js/firebase.js
+++ js/firebase.js
@@ -8,16 +8,16 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.14.0/firebase
 import { getStorage } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-storage.js";
 
 /**
- * @todo 재현님 파이어 베이스 추가하기
  * 아래 데이터는 본인의 Firebase 프로젝트 설정에서 확인할 수 있습니다.
+ * @see https://console.firebase.google.com/project/elevated-glow-332304/settings/general/web:Njc0MmI1NDYtMGNhNS00NTZhLWEzMzctZjgxMDcxMjEwMDdh
  */
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyD1BvEIkVvOBmcx405Al9scSfIAjRQo2UA",
+  authDomain: "elevated-glow-332304.firebaseapp.com",
+  projectId: "elevated-glow-332304",
+  storageBucket: "elevated-glow-332304.appspot.com",
+  messagingSenderId: "330312765599",
+  appId: "1:330312765599:web:1c08f8355d3848779912aa",
 };
 
 // Initialize Firebase
